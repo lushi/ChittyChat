04eaeb54224074cc913b6e32fa6bae08f89743f5
Lushi Li <lushili06@gmail.com>
2016-11-24 17:48:53 -0500

    Db.set() throws error if collection or data not provided in arguments

--- data/Db.js
+++ data/Db.js
@@ -2,8 +2,14 @@ const _ = require ('lodash');
 
 const Db = (() => {
   return {
-    set: () => {
+    set: (collection, data) => {
+      if (typeof collection === 'undefined' || collection === null) {
+        throw new Error('collection is not defined');
+      }
 
+      if (typeof data === 'undefined' || data === null) {
+        throw new Error('data is not defined');
+      }
     }
   }
 })();
--- test/Db.js
+++ test/Db.js
@@ -5,8 +5,13 @@ const expect = chai.expect;
 
 describe('Db', function() {
   describe('#set', function() {
-    it('should throw error if collection name is not provided', function() {
+    it('should throw error if collection name is not provided', () => {
       let fn = () => {Db.set(null, {foo: 'foo'})};
+      expect(fn).to.throw(Error);
+    })
+
+    it('should throw error if data is not provided', () => {
+      let fn = () => {Db.set('User', null)};
       expect(fn).to.throw(Error);
     })
   })
