3f692367c1d755201910bae6829a3923073c8a80
Lushi Li <lushili06@gmail.com>
2016-11-24 18:02:24 -0500

    use a copy of Db for testing so as not to pollute real data

--- test/Db.js
+++ test/Db.js
@@ -1,17 +1,18 @@
 const chai = require('chai');
-const Db = require('../data/Db.js');
+// make a copy of production database for testing purposes
+const testDb = Object.assign({}, require('../data/Db.js'));
 
 const expect = chai.expect;
 
 describe('Db', function() {
   describe('#set', function() {
     it('should throw error if collection name is not provided', () => {
-      let fn = () => {Db.set(null, {foo: 'foo'})};
+      let fn = () => {testDb.set(null, {foo: 'foo'})};
       expect(fn).to.throw(Error);
     })
 
     it('should throw error if data is not provided', () => {
-      let fn = () => {Db.set('User', null)};
+      let fn = () => {testDb.set('User', null)};
       expect(fn).to.throw(Error);
     })
   })
