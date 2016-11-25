dabeeeb84d43f80da06a7669552c6fd4c2468bb7
Lushi Li <lushili06@gmail.com>
2016-11-25 00:45:38 -0500

    flesh out Db.set() and also write Db.get() and Db.clear() in order to facilitate testing

--- data/Db.js
+++ data/Db.js
@@ -1,7 +1,18 @@
 const _ = require ('lodash');
 
 const Db = (() => {
+  let _data = {};
+
+  let _id = 0;
+  const _generateId = () => {
+    return _id++;
+  }
+
   return {
+    get: (collection) => {
+      return _data[collection];
+    },
+
     set: (collection, data) => {
       if (typeof collection === 'undefined' || collection === null) {
         throw new Error('collection is not defined');
@@ -10,6 +21,25 @@ const Db = (() => {
       if (typeof data === 'undefined' || data === null) {
         throw new Error('data is not defined');
       }
+
+      const now = Date.now();
+      const doc = Object.assign({}, data, {
+        _id: _generateId(),
+        creation_time: now,
+        modified_time: now
+      });
+
+      if (_data[collection])
+        _data[collection].push(doc);
+      else {
+        _data[collection] = [doc];
+      }
+
+      return doc;
+    },
+
+    clear: () => {
+      _data = {};
     }
   }
 })();
--- test/Db.js
+++ test/Db.js
@@ -1,19 +1,84 @@
 const chai = require('chai');
+const expect = chai.expect;
+
 // make a copy of production database for testing purposes
 const testDb = Object.assign({}, require('../data/Db.js'));
 
-const expect = chai.expect;
-
-describe('Db', function() {
-  describe('#set', function() {
+describe('Db', () => {
+  describe('#set', () => {
     it('should throw error if collection name is not provided', () => {
-      let fn = () => {testDb.set(null, {foo: 'foo'})};
+      const fn = () => {testDb.set(null, {foo: 'foo'})};
       expect(fn).to.throw(Error);
     })
 
     it('should throw error if data is not provided', () => {
-      let fn = () => {testDb.set('User', null)};
+      const fn = () => {testDb.set('User', null)};
       expect(fn).to.throw(Error);
     })
+
+    const collection = 'Message';
+    const data = {content: 'Hai', from: 'Mary', to: 'Joe'};
+    let now;
+
+    beforeEach(() => {
+      testDb.clear();
+      now = Date.now();
+      testDb.set(collection, data);
+    });
+
+    it('should insert document the given collection in the database', () => {
+      expect(testDb.get(collection).length).to.not.equal(0);
+    })
+
+    it('should include the given data in the inserted document', () => {
+      expect(testDb.get(collection)[0]).to.include.keys('content', 'from', 'to');
+    })
+
+    it('should include property "_id" in the inserted document', () => {
+      expect(testDb.get(collection)[0]).to.include.keys('_id');
+    })
+
+    it('should include a unique "_id" value in the inserted document', () => {
+      testDb.set(collection, {content: 'Hello', from: 'Joe', to: 'Mary'});
+
+      expect(testDb.get(collection)[0]._id).to.not.equal(testDb.get(collection)[1]._id);
+    })
+
+    it('should include property "creation_time" in the inserted document', () => {
+      expect(testDb.get(collection)[0]).to.include.keys('creation_time');
+    })
+
+    it('creation time should be time of when document was created', () => {
+      expect(testDb.get(collection)[0].creation_time).to.be.equal(now);
+    })
+
+    it('should return a document object with property "modified_time"', () => {
+      expect(testDb.get(collection)[0]).to.include.keys('modified_time');
+    })
+
+    it('"modified_time" value should equal "creation_time" value', () => {
+      const doc = testDb.get(collection)[0];
+      expect(doc.modified_time).to.be.equal(doc.creation_time);
+    })
+
+    it('should return a document object', () => {
+      const newDoc = testDb.set(collection, data);
+      expect(newDoc).to.be.a('object');
+    })
+
+    it('should append new document to existing documents in a given collection', () => {
+      testDb.set(collection, {content: 'Hello', from: 'Joe', to: 'Mary'});
+
+      expect(testDb.get(collection).length).to.be.equal(2);
+    })
   })
+
+  describe('#clear', () => {
+    it('should clear all data in the database', () => {
+      const doc = testDb.set('Message', {content: 'Hai', from: 'Mary', to: 'Joe'});
+      testDb.clear();
+
+      expect(testDb.get('Message')).to.not.exist;
+    })
+  });
 })
